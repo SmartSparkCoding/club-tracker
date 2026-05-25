1fa06021738e856f5163d822634108e283b971ef
Jacob Navaratne <jacob.navaratne@outlook.com>
2026-05-25 15:34:48 +0000

    add firebase, login screen then some basic styling i found on the workshop thingy!

---
+++ static/script.js
@@ -0,0 +1,50 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAOIMrsepVj9PLubDYAeUA-Qowse2GlTXI",
+  authDomain: "sterlo-kids-banking.firebaseapp.com",
+  databaseURL: "https://sterlo-kids-banking-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "sterlo-kids-banking",
+  storageBucket: "sterlo-kids-banking.firebasestorage.app",
+  messagingSenderId: "39217932124",
+  appId: "1:39217932124:web:be09cd44487c4b904a8399"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// -------------------------------------------
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        // User is signed in.
+        document.getElementById('user_div').style.display = 'block'
+        document.getElementById('login_div').style.display = 'none'
+    }
+    else {
+        // No user is signed in.
+        document.getElementById('user_div').style.display = 'none'
+        document.getElementById('login_div').style.display = 'block'
+    }   
+})
+
+function login() {
+  var userEmail = document.getElementById('email_field').value
+  var userPass = document.getElementById('password_field').value
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(userEmail, userPass)
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code
+      var errorMessage = error.message
+
+      window.alert('Error : ' + errorMessage)
+
+      // ...
+    })
+}
+
+function logout() {
+  firebase.auth().signOut()
+}
